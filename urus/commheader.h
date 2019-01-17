dd609038f1f2768f8d4215b226fc9529880f614e
lamborcai <lamborcai@gmail.com>
2019-01-17 13:37:09 +0800

    修改：     .gitignore 	修改：     LICENSE 	新文件：   urus/commheader.h 	新文件：   urus/images/icon.rc 	新文件：   urus/images/logo.ico 	新文件：   urus/main.cpp 	新文件：   urus/mainwindow.cpp 	新文件：   urus/mainwindow.h 	新文件：   urus/urus.pro

---
+++ urus/commheader.h
@@ -0,0 +1,14 @@
+#ifndef COMMHEADER_H
+#define COMMHEADER_H
+
+//General Methods
+#include <QCoreApplication>
+
+//UI lanbery
+#include <QLabel>
+#include <QDateTime>
+#include <QTextEdit>
+
+
+
+#endif // COMMHEADER_H
